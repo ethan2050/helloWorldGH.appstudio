4787cb13554413fcab86d4070b206d3d0085c458
ethan2050 <ejm60549@creighton.edu>
2020-09-15 08:39:36 -0500

    Initial commit

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,32 @@
+/*
+Git is a software that allows you to keep track of changes made to a 
+program/project over time. Git works by recording the changes you make 
+to a project - like a snapshot of your program at a given time. It stores
+these. Then you can reference them, decide to make them a part of the 
+'master' program/project, revert back to a previous one, etc. 
+
+Git projects are usually managed on Github.com, a website that hosts 
+Git projects. There you can access your projects by using the basic workflow
+we'll be using and about which you read.
+
+OK - here are the steps. I'll clarify terminology as we go along. 
+1. Resave this appStudio project in a folder named Sept9GitHub and name it 
+'HelloWorldGH'. Rename Form1 'hello'. Now run the project to make sure
+there are not any problems. 
+*/
+
+/*
+//this is a small sample program to test with GitHub
+
+alert("Hello World")
+*/
+
+/*
+2. Now open GitHub Desktop. Click on Current Repository - then Create New Repository. 
+This creats a new (local) GH project that will contain your appStudio project. 
+** You must name the new repository with the same name as the appStudio project, including
+the .appstudio file extension. 
+So name the new repository: 'helloWorldGH.appstudio'. 
+
+
+*/
